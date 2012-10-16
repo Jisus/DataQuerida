7ede345c83072e926a8ad6326d5697c288b3afe8
Rodrigo Dias <rodrigo.dias@planb.com.br>
2012-10-16 20:58:01 -0300

    Commit Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dataquerida::Application.config.secret_token = '037cb99ce71de46bb225a00520e037ecefa41924d8f5290887169275fb4e285ee792c4c897546fde136e8fed2f20bb5b70848e968c72607cf10c3803bb1b42b8'
